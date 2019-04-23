50f4acf23a94724037326dc382b8d44b0137aee8
Marcos Oliveira <2160820@my.ipleiria.pt>
2019-04-23 02:02:37 +0100

    Update database.py
    Adicionado o código sql para criar as tabelas necessárias, incluindo a tabela cpu (que tem uma nova estrutura).

--- database.py
+++ database.py
@@ -7,15 +7,63 @@ database = dirname + "\\database\\database.db"
 def main():
 	dirname = os.path.dirname(__file__)
 	database = dirname + "\\database\\database.db"
-	sql_create_cpu_table = ''' CREATE TABLE IF NOT EXISTS cpu (id INTEGER PRIMARY KEY, cpu_usage REAL NOT NULL, date_time TEXT)'''
+
+	sql_create_jobs_table = '''CREATE TABLE IF NOT EXISTS jobs
+							(
+								id INTEGER PRIMARY KEY AUTOINCREMENT,
+								start_time TEXT NOT NULL,
+								finish_time TEXT NOT NULL
+							)'''
+
+	sql_create_updates_table = '''CREATE TABLE IF NOT EXISTS updates
+							(
+								id INTEGER PRIMARY KEY AUTOINCREMENT,
+								job_id INTEGER NOT NULL,
+								timedate TEXT NOT NULL,
+								FOREIGN KEY(job_id) REFERENCES jobs(id)
+							)'''
+
+	sql_create_cpu_table = ''' CREATE TABLE IF NOT EXISTS cpu
+							(
+								id INTEGER PRIMARY KEY,
+								usage_percentage REAL NOT NULL,
+								affinity INTEGER NOT NULL,
+								threads INTEGER NOT NULL,
+								cpu_time REAL NOT NULL,
+								idle_time REAL NOT NULL,
+								FOREIGN KEY(id) REFERENCES updates(id)
+							)'''
+
+	sql_create_IO_table = '''CREATE TABLE IF NOT EXISTS IO
+							(
+								id INTEGER PRIMARY KEY,
+								read_count INTEGER NOT NULL,
+								write_count INTEGER NOT NULL,
+								read_bytes INTEGER NOT NULL,
+								write_bytes INTEGER NOT NULL,
+								FOREIGN KEY(id) REFERENCES updates(id)
+							)'''
+
+	sql_create_memory_table = '''CREATE TABLE IF NOT EXISTS memory
+							(
+								id INTEGER PRIMARY KEY,
+								memory_usage INTEGER NOT NULL,
+								page_faults INTEGER NOT NULL,
+								FOREIGN KEY(id) REFERENCES updates(id)
+							)'''
+
 	conn = create_connection(database)
 	if conn is not None:
+		create_table(conn, sql_create_jobs_table)
+		create_table(conn, sql_create_updates_table)
 		create_table(conn, sql_create_cpu_table)
+		create_table(conn, sql_create_IO_table)
+		create_table(conn, sql_create_memory_table)
 	else:
 		print("Error! cannot create a database connection")
 
 
-def create_connection(db_file):
+def create_connection(db_file): 
     try:
         conn = sqlite3.connect(db_file)
         return conn
@@ -30,7 +78,6 @@ def create_table(conn, create_table_sql):
         c.execute(create_table_sql)
     except sqlite3.Error as e:
         print(e)
-
 
 #EXECUTION
 if __name__ == "__main__":
